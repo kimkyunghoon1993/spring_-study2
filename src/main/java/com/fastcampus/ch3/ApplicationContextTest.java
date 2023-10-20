33df69c90c9a8e7346072831304e8caa7b3befee
김경훈 <kimkyunghoon1993@gmail.com>
2023-10-20 17:29:26 +0900

    2023-10-20 금요일

---
+++ src/main/java/com/fastcampus/ch3/ApplicationContextTest.java
@@ -0,0 +1,71 @@
+package com.fastcampus.ch3;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+class Door {
+}
+
+@Component
+class Engine {
+}
+
+@Component
+class TurboEngine extends Engine {
+}
+
+@Component
+class SuperEngine extends Engine {
+}
+
+@Component
+class Car {
+    @Value("red")
+    String color;
+    @Value("100")
+    int oil;
+    // @Autowired
+    Engine engine;
+    // @Autowired
+    Door[] doors;
+
+    public Car(){
+
+    }
+    @Autowired
+    public Car(@Value("red") String color, @Value("100") int oil, Engine engine, Door[] doors) {
+        this.color = color;
+        this.oil = oil;
+        this.engine = engine;
+        this.doors = doors;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+            "color='" + color + '\'' +
+            ", oil=" + oil +
+            ", engine=" + engine +
+            ", doors=" + Arrays.toString(doors) +
+            '}';
+    }
+}
+
+public class ApplicationContextTest {
+    public static void main(String[] args) {
+        ApplicationContext ac = new GenericXmlApplicationContext("config.xml");
+        //      Car car = ac.getBean("car", Car.class); // 타입을 지정하면 형변환 안해도됨. 아래의 문장과 동일
+        Car car = (Car)ac.getBean("car"); // 이름으로 빈 검색
+        Car car2 = (Car)ac.getBean(Car.class);   // 타입으로 빈 검색
+        System.out.println("car = " + car);
+
+    }
+}
