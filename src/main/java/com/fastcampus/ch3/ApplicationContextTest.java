e134fac6195914998ba49c614fec02fa650fb7ea
김경훈 <kimkyunghoon1993@gmail.com>
2023-10-23 17:27:04 +0900

    2023-10-23 월요일 DB 연결및 TDD 실습 15강 시청중

--- src/main/java/com/fastcampus/ch3/ApplicationContextTest.java
+++ src/main/java/com/fastcampus/ch3/ApplicationContextTest.java
@@ -1,10 +1,13 @@
 package com.fastcampus.ch3;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.stereotype.Component;
@@ -59,6 +62,32 @@ class Car {
     }
 }
 
+@Component
+@PropertySource("setting.properties")
+class SysInfo{
+    @Value("#{systemProperties['user.timezone']})")
+    String timeZone;
+    @Value("#{systemEnvironment['PWD']}")
+    String currDir;
+    @Value("${autosaveDir}")
+    String autosaveDir;
+    @Value("${autosaveInterval}")
+    int autosaveInterval;
+    @Value("${autosave}")
+    boolean autosave;
+
+    @Override
+    public String toString() {
+        return "SysInfo{" +
+            "timeZone='" + timeZone + '\'' +
+            ", currDir='" + currDir + '\'' +
+            ", autosaveDir='" + autosaveDir + '\'' +
+            ", autosaveInterval=" + autosaveInterval +
+            ", autosave=" + autosave +
+            '}';
+    }
+}
+
 public class ApplicationContextTest {
     public static void main(String[] args) {
         ApplicationContext ac = new GenericXmlApplicationContext("config.xml");
@@ -67,5 +96,11 @@ public class ApplicationContextTest {
         Car car2 = (Car)ac.getBean(Car.class);   // 타입으로 빈 검색
         System.out.println("car = " + car);
 
+        System.out.println("ac.getBean(SysInfo.class) = " + ac.getBean(SysInfo.class));
+        Map<String,String> map = System.getenv();
+        System.out.println("map = " + map);
+
+        Properties properties = System.getProperties();
+        System.out.println("properties = " + properties);
     }
 }
